95ea1395eb22f3e461d3d249e5a8b4fb9307c204
joseph andrade <jandrade@dnamicro.com>
2016-08-05 17:13:16 +0800

    refractor

--- DialerApp/app/src/main/java/com/example/dna/dialerapp/SipDroid.java
+++ DialerApp/app/src/main/java/com/example/dna/dialerapp/SipDroid.java
@@ -8,4 +8,11 @@ import android.net.sip.SipProfile;
 /**
  * Created by dna on 8/5/16.
  */
+public class SipDroid {
+    public SipManager manager = null;
+    public SipProfile me = null;
+    public SipAudioCall call = null;
+    //public IncomingCallReceiver callReceiver;
 
+
+}
